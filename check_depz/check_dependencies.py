28eb7380d086a7fa10a24b8f33a4b0bb395575f9
Henryk Konsek <hekonsek@gmail.com>
2025-03-27 08:07:24 +0100

    Added all dependencies check.

--- check_depz/check_dependencies.py
+++ check_depz/check_dependencies.py
@@ -2,26 +2,33 @@ import subprocess
 import sys
 import shlex
 
-def main():
+def check_poetry_output(name: str, command: str, limit: int) -> bool:
     result = subprocess.run(
-        shlex.split("poetry show  --outdated --top-level"), 
+        shlex.split(command), 
         capture_output=True, text=True
     )
     outdated_packages = result.stdout.strip().split("\n")
     outdated_count = len(outdated_packages) - 1
 
-    print(f"Number of outdated dependencies: {outdated_count}")
-    print()
-    print(result.stdout)
+    print(f"Outdated {name} dependencies: {outdated_count}")
+    if len(result.stdout) > 0:
+        print()
+        print(result.stdout)
 
-    limit = 10
     if outdated_count > limit:
-        print(f"Too many outdated dependencies (limit is {limit}). Check failed.")
-        sys.exit(1)
+        print(f"Too many outdated {name} dependencies (limit is {limit}). Check failed.")
+        return False
     else:
-        print("Dependency check passed.")
-        sys.exit(0)
+        return True
 
+def main():
+    top_level_result = check_poetry_output("top level", "poetry show --outdated --top-level", 10)
+    all_result = check_poetry_output("all", "poetry show --outdated", 20)
+    if top_level_result and all_result:
+        print("All dependency checks passed.")
+        sys.exit(0)
+    else:
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
